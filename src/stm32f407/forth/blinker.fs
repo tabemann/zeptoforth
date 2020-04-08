99b9256b4f42e0159c6cc5a4e93ce6095e1664b2
Travis Bemann <tabemann@gmail.com>
2020-04-07 21:00:45 -0500

    Added a blinky that takes advantage of the F407 DISCOVERY's four user-controllable LEDs

---
+++ src/stm32f407/forth/blinker.fs
@@ -0,0 +1,62 @@
+\ Copyright (c) 2020 Travis Bemann
+\
+\ This program is free software: you can redistribute it and/or modify
+\ it under the terms of the GNU General Public License as published by
+\ the Free Software Foundation, either version 3 of the License, or
+\ (at your option) any later version.
+\
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+\
+\ You should have received a copy of the GNU General Public License
+\ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+\ Compile this to flash
+compile-to-flash
+
+\ The blinker delay time
+variable blinker-delay
+
+\ The blinker
+: blinker ( -- )
+  led-red-on
+  begin
+    pause
+    blinker-delay @ ms
+    led-red-off
+    led-orange-on
+    pause
+    blinker-delay @ ms
+    led-orange-off
+    led-green-on
+    pause
+    blinker-delay @ ms
+    led-green-off
+    led-blue-on
+    pause
+    blinker-delay @ ms
+    led-blue-off
+    led-red-on
+  again
+;
+
+
+\ The blinker task
+variable blinker-task
+
+\ Init
+: init ( -- )
+  init
+  led-red-off
+  led-orange-off
+  led-green-off
+  led-blue-off
+  500 blinker-delay !
+  ['] blinker 256 256 256 spawn blinker-task !
+  blinker-task @ enable-task
+;
+
+\ Reboot to initialize
+reboot
