7afc11348fe8a6adfb7a6df65bedaf12f6afc6a1
Travis Bemann <tabemann@gmail.com>
2020-09-06 18:35:31 -0500

    Added new binaries to be built comprising mini binaries implementing a limited set of zeptoforth functionality

--- utils/make_image.sh
+++ utils/make_image.sh
@@ -18,14 +18,15 @@
 VERSION=$1
 PLATFORM=$2
 PORT=$3
+IMAGE=$4
 PROJECT=zeptoforth
 
 rm screenlog.0
 st-flash erase
 st-flash write bin/$VERSION/$PLATFORM/zeptoforth_kernel-$VERSION.bin 0x08000000
 st-flash reset
-sleep 4
-./utils/codeload3.py -B 115200 -p $PORT serial src/$PLATFORM/forth/setup.fs
+sleep 3
+./utils/codeload3.py -B 115200 -p $PORT serial src/$PLATFORM/forth/setup_$IMAGE.fs
 ./utils/codeload3.py -B 115200 -p $PORT serial src/common/forth/ihex.fs
 screen -d -m $PORT 115200
 screen -X log on
@@ -38,7 +39,7 @@ sed '1d' screenlog.0 > inter
 sed '$d' inter > inter.1
 sed '$d' inter.1 > inter
 sed 's/:00000001FF clone_end/:00000001FF/' inter > inter.1
-mv inter.1 bin/$VERSION/$PLATFORM/zeptoforth_full-$VERSION.ihex
-arm-none-eabi-objcopy -I ihex -O binary bin/$VERSION/$PLATFORM/zeptoforth_full-$VERSION.ihex bin/$VERSION/$PLATFORM/zeptoforth_full-$VERSION.bin
+mv inter.1 bin/$VERSION/$PLATFORM/zeptoforth_$IMAGE-$VERSION.ihex
+arm-none-eabi-objcopy -I ihex -O binary bin/$VERSION/$PLATFORM/zeptoforth_$IMAGE-$VERSION.ihex bin/$VERSION/$PLATFORM/zeptoforth_$IMAGE-$VERSION.bin
 rm screenlog.0
 rm inter
