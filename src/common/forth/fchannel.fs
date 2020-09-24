edfc903c4480e5274ee55ac51e586c38d1fb5def
Travis Bemann <tabemann@gmail.com>
2020-09-24 09:44:59 -0500

    Added the beginnings of fast channels

---
+++ src/common/forth/fchannel.fs
@@ -0,0 +1,90 @@
+\ Copyright (c) 2020 Travis Bemann
+\
+\ Permission is hereby granted, free of charge, to any person obtaining a copy
+\ of this software and associated documentation files (the "Software"), to deal
+\ in the Software without restriction, including without limitation the rights
+\ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+\ copies of the Software, and to permit persons to whom the Software is
+\ furnished to do so, subject to the following conditions:
+\ 
+\ The above copyright notice and this permission notice shall be included in
+\ all copies or substantial portions of the Software.
+\ 
+\ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+\ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+\ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+\ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+\ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+\ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+\ SOFTWARE.
+
+\ Set up the wordlist
+forth-wordlist 1 set-order
+forth-wordlist set-current
+
+\ Check whether this is already defined
+defined? fchan-wordlist not [if]
+
+  \ Setup the wordlist
+  wordlist constant fchan-wordlist
+  wordlist constant fchan-internal-wordlist
+  forth-wordlist task-wordlist fchan-internal-wordlist fchan-wordlist
+  4 set-order
+  fchan-wordlist set-current
+
+  \ Fchannel header structure
+  begin-structure fchan-header-size
+
+    \ Switch the current wordlist
+    fchan-internal-wordlist set-current
+
+    \ Fchannel receive task
+    field: fchan-recv-task
+
+    \ Fchannel send task
+    field: fchan-send-task
+
+    \ Fchannel sent data address
+    field: fchan-send-addr
+
+    \ Fchannel sent data count
+    field: fchan-send-count
+    
+  end-structure
+
+  \ Switch the current wordlist
+  fchan-wordlist set-current
+
+  \ Send data on an fchannel
+  : send-fchan ( addr bytes fchan -- )
+    begin dup fchan-send-task @ 0<> while
+      pause
+    repeat
+    current-task over fchan-send-task !
+    tuck fchan-send-count !
+    tuck fchan-send-addr !
+    dup fchan-recv-task @ 0<> if
+      fchan-recv-task @ enable-task
+    then
+    current-task @ disable-task
+    pause
+  ;
+
+  \ Receive data on an fchannel
+  : recv-fchan ( fchan -- addr bytes )
+    begin dup fchan-recv-task @ 0<> while
+      pause
+    repeat
+    begin dup fchan-send-task @ 0<> while
+      current-task over fchan-recv-task !
+      current-task disable-task
+      pause
+    repeat
+    dup fchan-send-task @ enable-task
+    0 over fchan-recv-task @ !
+    dup fchan-send-addr @
+    swap fchan-send-count @
+  ;
+
+[then]
+    
