da73fd13606c74bd04a74fedf30465114896e65a
Travis Bemann <tabemann@gmail.com>
2020-09-25 09:26:49 -0500

    Some fchannel-related stuff

--- src/common/forth/fchannel.fs
+++ src/common/forth/fchannel.fs
@@ -32,22 +32,22 @@ defined? fchan-wordlist not [if]
   4 set-order
   fchan-wordlist set-current
 
-  \ Fchannel header structure
-  begin-structure fchan-header-size
+  \ Fast channel header structure
+  begin-structure fchan-size
 
     \ Switch the current wordlist
     fchan-internal-wordlist set-current
 
-    \ Fchannel receive task
+    \ Fast channel receive task
     field: fchan-recv-task
 
-    \ Fchannel send task
+    \ Fast channel send task
     field: fchan-send-task
 
-    \ Fchannel sent data address
+    \ Fast channel sent data address
     field: fchan-send-addr
 
-    \ Fchannel sent data count
+    \ Fast channel sent data count
     field: fchan-send-count
     
   end-structure
@@ -55,37 +55,68 @@ defined? fchan-wordlist not [if]
   \ Switch the current wordlist
   fchan-wordlist set-current
 
-  \ Send data on an fchannel
+  \ Initialize an fast channel
+  : init-fchan ( addr -- )
+    0 over fchan-recv-task !
+    0 over fchan-send-task ! 
+    0 over fchan-send-addr !
+    0 swap fchan-send-count !
+  ;
+
+  \ Send data on an fast channel
   : send-fchan ( addr bytes fchan -- )
+    0 pause-enabled ! cr ." @A" 1 pause-enabled !
     begin dup fchan-send-task @ 0<> while
       pause
+      0 pause-enabled ! cr ." @B" 1 pause-enabled !
     repeat
+    0 pause-enabled ! cr ." @C" 1 pause-enabled !
     current-task over fchan-send-task !
     tuck fchan-send-count !
     tuck fchan-send-addr !
     dup fchan-recv-task @ 0<> if
-      fchan-recv-task @ enable-task
+      0 pause-enabled ! cr ." @D" 1 pause-enabled !
+      fchan-recv-task @ activate-task
     then
-    current-task @ disable-task
+    0 pause-enabled ! cr ." @E" 1 pause-enabled !
+    current-task disable-task
+    0 pause-enabled ! cr ." @F" 1 pause-enabled !
     pause
+    0 pause-enabled ! cr ." @G" 1 pause-enabled !
   ;
 
-  \ Receive data on an fchannel
+  \ Receive data on an fast channel
   : recv-fchan ( fchan -- addr bytes )
+    0 pause-enabled ! cr dup h.8 1 pause-enabled !
+    0 pause-enabled ! cr ." *A" 1 pause-enabled !
     begin dup fchan-recv-task @ 0<> while
+      0 pause-enabled ! cr ." *AA" 1 pause-enabled !
       pause
+      0 pause-enabled ! cr ." *B" 1 pause-enabled !
     repeat
+    0 pause-enabled ! cr ." *BB" 1 pause-enabled !
     begin dup fchan-send-task @ 0= while
+      0 pause-enabled ! cr ." *C" 1 pause-enabled !
       current-task over fchan-recv-task !
       current-task disable-task
+      0 pause-enabled ! cr ." *D" 1 pause-enabled !
       pause
+      0 pause-enabled ! cr ." *E" 1 pause-enabled !
     repeat
+    0 pause-enabled ! cr ." *F" 1 pause-enabled !
     dup fchan-send-task @ enable-task
     0 over fchan-send-task !
     0 over fchan-recv-task !
     dup fchan-send-addr @
     swap fchan-send-count @
+    0 pause-enabled ! cr ." *G" 1 pause-enabled !
   ;
+
+  \ Send a cell on an fast channel
+  : send-fchan-cell ( x fchan -- ) 0 swap send-fchan ;
+
+  \ Receive a cell from an fast channel
+  : recv-fchan-cell ( fchan -- x ) recv-fchan drop ;
 
 [then]
     
