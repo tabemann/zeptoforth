ffeb027293699e3245cd2f26d4d110f1e7d3ce66
Travis Bemann <tabemann@gmail.com>
2021-09-26 21:44:32 -0500

    Still bringing up zeptoforth on the RP2040

--- src/rp2040/forth/setup_mini_swdcom.fs
+++ src/rp2040/forth/setup_mini_swdcom.fs
@@ -20,15 +20,15 @@
 
 \ This is not actual Forth code, but rather setup directives for e4thcom to be
 \ executed from the root of the zeptoforth directory to initialize zeptoforth
-\ on an STM32F407 device.
+\ on an RP2040 device.
 
-#include src/stm32f407/forth/clock.fs
+\ #include src/rp2040/forth/clock.fs
 #include src/common/forth/basic.fs
 #include src/common/forth/module.fs
 #include src/common/forth/interrupt.fs
-#include src/stm32f407/forth/erase.fs
+#include src/rp2040/forth/erase.fs
 #include src/common/forth/systick.fs
-#include src/stm32f407/forth/int_io.fs
-#include src/stm32f407/forth/gpio.fs
+#include src/rp2040/forth/int_io.fs
+\ #include src/rp2040/forth/gpio.fs
 #include src/common/forth/task.fs
 #include src/common/forth/swdcom.fs
