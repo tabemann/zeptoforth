46129e164e8a153ac89b44ea52d1284b4f315796
Travis Bemann <tabemann@gmail.com>
2022-02-25 21:29:09 -0600

    Added source code examples for the presentation

---
+++ test/rp2040/present/chan_test.fs
@@ -0,0 +1,30 @@
+\ Copyright (c) 2022 Travis Bemann
+\ 
+\ Permission is hereby granted, free of charge, to any person obtaining a copy
+\ of this software and associated documentation files (the "Software"), to deal
+\ in the Software without restriction, including without limitation the rights
+\ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+\ copies of the Software, and to permit persons to whom the Software is
+\ furnished to do so, subject to the following conditions:
+\ 
+\ The above copyright notice and this permission notice shall be included in
+\ all copies or substantial portions of the Software.
+\ 
+\ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+\ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+\ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+\ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+\ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+\ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+\ SOFTWARE.
+
+task import
+chan import
+64 constant element-count
+cell element-count chan-size buffer: my-chan
+: init-test ( -- )
+  cell element-count my-chan init-chan
+  0 [: begin [: my-chan recv-chan ;] extract-allot-cell . again ;]
+  320 128 512 0 spawn-on-core run
+  0 [: 0 begin dup [: my-chan send-chan ;] provide-allot-cell 1+ 1000 ms again ;]
+  320 128 512 1 spawn-on-core run ;
