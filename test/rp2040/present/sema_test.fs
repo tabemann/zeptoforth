edb9f917ecddbb3835af5789a87848ee7eaa7c18
Travis Bemann <tabemann@gmail.com>
2022-02-25 21:29:09 -0600

    Added source code examples for the presentation

---
+++ test/rp2040/present/sema_test.fs
@@ -0,0 +1,27 @@
+\ Copyright (c) 2022 Travis Bemann
+\ 
+\ Permission is hereby granted, free of charge, to any person obtaining a copy
+\ of this software and associated documentation files (the "Software"), to deal
+\ in the Software without restriction, including without limitation the rights
+\ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+\ copies of the Software, and to permit persons to whom the Software is
+\ furnished to do so, subject to the following conditions:
+\ 
+\ The above copyright notice and this permission notice shall be included in
+\ all copies or substantial portions of the Software.
+\ 
+\ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+\ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+\ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+\ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+\ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+\ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+\ SOFTWARE.
+
+task import
+sema import
+sema-size buffer: my-sema
+: init-test ( -- )
+  1 0 my-sema init-sema \ This is a binary semaphore, as indicated by the limit of 1
+  0 [: begin my-sema take ." *" again ;] 320 128 512 0 spawn-on-core run
+  0 [: begin my-sema give 1000 ms again ;] 320 128 512 1 spawn-on-core run ;
