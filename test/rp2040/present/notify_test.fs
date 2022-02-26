84c8daf21ccaf176b445ec3e979429c7125bafad
Travis Bemann <tabemann@gmail.com>
2022-02-25 21:29:09 -0600

    Added source code examples for the presentation

---
+++ test/rp2040/present/notify_test.fs
@@ -0,0 +1,30 @@
+\ Copyright (c) 2022 Travis Bemann
+\ 
+\ Permission is hereby granted, free of charge, to any person obtaining a copy
+\ of this software and associated documentation files (the "Software"), to deal
+\ in the Software without restriction, including without limitation the rights
+\ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+\ copies of the Software, and to permit persons to whom the Software is
+\ furnished to do so, subject to the following conditions:
+\ 
+\ The above copyright notice and this permission notice shall be included in
+\ all copies or substantial portions of the Software.
+\ 
+\ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+\ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+\ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+\ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+\ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+\ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+\ SOFTWARE.
+
+task import
+1 constant notify-count
+notify-count cells buffer: notify-area
+variable notified-task
+: init-test ( -- )
+  0 [: begin 0 wait-notify . again ;] 320 128 512 0 spawn-on-core notified-task !
+  notify-area notify-count notified-task @ config-notify -1 0 notified-task @ mailbox!
+  notified-task @ run
+  0 [: begin ['] 1+ 0 notified-task @ notify-update 1000 ms again ;]
+  320 128 512 1 spawn-on-core run ;
