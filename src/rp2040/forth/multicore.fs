7677c951da898b9efdf1c1dc39fea2ac01e8c31f
Travis Bemann <tabemann@gmail.com>
2022-03-04 22:43:13 -0600

    Got erasing while the second core of the RP2040 is running working

--- src/rp2040/forth/multicore.fs
+++ src/rp2040/forth/multicore.fs
@@ -262,12 +262,12 @@ begin-module multicore
   \ Prepare for rebooting the second core
   : prepare-reboot ( -- )
     cpu-index 0 = averts x-core-0-only
-    SIO_IRQ_PROC0 ^ interrupt :: NVIC_ICER_CLRENA!
+\    SIO_IRQ_PROC0 ^ interrupt :: NVIC_ICER_CLRENA!
     force-core-reboot
 \    PSM_FRCE_OFF_PROC1 PSM_FRCE_OFF bis!
 \    begin PSM_FRCE_OFF_PROC1 PSM_FRCE_OFF bit@ until
-\    PSM_FRCE_OFF_PROC1 PSM_FRCE_OFF bic!
-    false core-1-launched !
+\    PSM_FRCE_ON_PROC1 PSM_FRCE_ON bis!
+\    false core-1-launched !
 \    1 fifo-pop-blocking drop
     spinlock-count 0 ?do -1 i SPINLOCK ! loop
   ;    
