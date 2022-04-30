fc80d7220939c6ba539c8843b4a7898c5a79be5c
Travis Bemann <tabemann@gmail.com>
2022-04-30 17:18:44 -0500

    Made a minor fix to the disassembler and added a simple Brainfuck test

---
+++ test/common/brainfuck_test.fs
@@ -0,0 +1,40 @@
+\ Copyright (c) 2022 Travis Bemann
+\
+\ Permission is hereby granted, free of charge, to any person obtaining a copy
+\ of this software and associated documentation files (the "Software"), to deal
+\ in the Software without restriction, including without limitation the rights
+\ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+\ copies of the Software, and to permit persons to whom the Software is
+\ furnished to do so, subject to the following conditions:
+\ 
+\ The above copyright notice and this permission notice shall be included in
+\ all copies or substantial portions of the Software.
+\ 
+\ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+\ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+\ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+\ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+\ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+\ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+\ SOFTWARE
+
+#include test/common/brainfuck.fs
+
+continue-module forth
+
+  brainfuck import
+
+  30000 buffer: my-buffer
+
+  \ The word test in Brainfuck ( addr -- addr' )
+  :bf test
+    , ---- ---- ---- ---- ---- ---- ---- ----
+    [->++<]
+    > ++++ ++++ ++++ ++++ ++++ ++++ ++++ ++++
+    . <
+  ;
+  
+  \ Start the test
+  : init-test ( -- ) my-buffer 30000 0 fill my-buffer test drop ;
+  
+end-module
