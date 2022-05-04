0493d1ec1787b2f8a628913a8cdcfcbc51393789
Travis Bemann <tabemann@gmail.com>
2022-05-04 00:56:01 -0500

    Updated the action scheduler to handle sending between larger numbers of actions better and added a test to test this.

--- test/common/action_multi_speed.fs
+++ test/common/action_multi_speed.fs
@@ -23,28 +23,38 @@ continue-module forth
   task import
   systick import
   action import
+  tinymt32 import
 
   \ Our schedule
   schedule-size buffer: my-schedule
 
+  \ Our PRNG
+  tinymt32-size buffer: my-tinymt32
+
+  \ Our count of consumers and producers
+  32 constant my-count
+
   \ Our actions
-  action-size buffer: consumer-action
-  action-size buffer: producer-action
+  action-size my-count * buffer: consumer-action
+  action-size my-count * buffer: producer-action
 
   \ The consumer buffer
-  cell buffer: consumer-buf
+  my-count cells buffer: consumer-buf
   
   \ Our deferred consumer routine
   defer consumer
 
   \ The consumer routine
   : do-consumer ( -- )
-    [: 2drop drop consumer ;] consumer-buf cell recv-action
+    [: 2drop drop consumer ;] current-data cells consumer-buf + cell recv-action
   ;
 
   \ Set the deferred consumer routine
   ' do-consumer ' consumer defer!
 
+  \ The producer buffer
+  my-count cells buffer: producer-buf
+  
   \ The producer buffer
   variable send-count
   
@@ -59,6 +69,7 @@ continue-module forth
 
   \ The producer routine
   : do-producer ( -- )
+    send-count @ producer-buf current-data cells + !
     [:
       1 send-count +!
       send-count @ send-count-limit > if
@@ -69,21 +80,30 @@ continue-module forth
 	start-systick !
       then
       producer
-    ;] send-count cell consumer-action send-action
+    ;] producer-buf current-data cells + cell
+    consumer-action my-tinymt32 tinymt32-generate-uint32 my-count umod
+    action-size * +
+    send-action
   ;
 
   \ Set the deferred producer routine
   ' do-producer ' producer defer!
 
   \ Initialize the test
-  : init-test ( -- )
+  : init-test ( seed -- )
+    my-tinymt32 tinymt32-prepare-example
+    my-tinymt32 tinymt32-init
     0 send-count !
     systick-counter start-systick !
     my-schedule init-schedule
-    0 ['] consumer consumer-action init-action
-    0 ['] producer producer-action init-action
-    my-schedule consumer-action add-action
-    my-schedule producer-action add-action
+    my-count 0 ?do
+      i ['] consumer i action-size * consumer-action + init-action
+      i ['] producer i action-size * producer-action + init-action
+    loop
+    my-count 0 ?do
+      my-schedule i action-size * consumer-action + add-action
+      my-schedule i action-size * producer-action + add-action
+    loop
     0 [: my-schedule run-schedule ;] 420 128 512 spawn run
   ;
   
