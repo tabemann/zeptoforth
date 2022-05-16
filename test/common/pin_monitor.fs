f69ad5c73adf359f76078c5d24ebe2e8ae04a641
Travis Bemann <tabemann@gmail.com>
2022-05-15 20:11:58 -0500

    Built release 0.36.0, which revamps the PIO API for the RP2040

--- test/common/pin_monitor.fs
+++ test/common/pin_monitor.fs
@@ -1,5 +1,5 @@
-\ Copyright (c) 2021-2022 Travis Bemann
-\ 
+\ Copyright (c) 2020-2022 Travis Bemann
+\
 \ Permission is hereby granted, free of charge, to any person obtaining a copy
 \ of this software and associated documentation files (the "Software"), to deal
 \ in the Software without restriction, including without limitation the rights
@@ -20,52 +20,13 @@
 
 continue-module forth
 
-  interrupt import
-  gpio import
-  pio import
-
-  \ The initial setup
-  create pio-init
-  1 SET_PINDIRS set,
-  0 SET_PINS set,
+  pin import
+  led import
+  task import
   
-  \ The PIO code
-  create pio-code
-  PULL_BLOCK PULL_NOT_EMPTY pull,
-  32 OUT_X out,
-  1 SET_PINS set,
-  3 COND_X1- jmp,
-  PULL_BLOCK PULL_NOT_EMPTY pull,
-  32 OUT_X out,
-  0 SET_PINS set,
-  7 COND_X1- jmp,
-
-  \ The blinker rate
-  variable blinker-vary
-
-  \ PIO interrupt handler
-  : handle-pio ( -- )
-    blinker-vary @ 0 PIO0 sm-txf!
-    PIO0_IRQ0 NVIC_ICPR_CLRPEND!
+  \ Monitor a pin
+  : pin-monitor ( pin -- )
+    1 [: begin dup pin@ green led! pause again ;] 256 128 512 spawn run
   ;
-
-  \ Init blinker
-  : init-blinker ( -- )
-    500 blinker-vary !
-    %0001 PIO0 sm-disable
-    %0001 PIO0 sm-restart
-    0 62500 0 PIO0 sm-clkdiv!
-    25 1 0 PIO0 sm-set-pins!
-    0 7 0 PIO0 sm-wrap!
-    on 0 PIO0 sm-out-sticky!
-    pio-init 2 0 PIO0 sm-instr!
-    pio-code 8 PIO0 pio-instr-mem!
-    0 0 PIO0 sm-addr!
-    blinker-vary @ 0 PIO0 sm-txf!
-    ['] handle-pio PIO0_IRQ0 16 + vector!
-    0 INT_SM_TXNFULL IRQ0 PIO0 pio-interrupt-enable
-    PIO0_IRQ0 NVIC_ISER_SETENA!
-    %0001 PIO0 sm-enable
-  ;
-
+  
 end-module
