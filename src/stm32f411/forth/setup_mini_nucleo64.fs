55717a648dde549399d29732c7ce9b378c182eca
Travis Bemann <tabemann@gmail.com>
2022-06-11 10:35:22 -0500

    Added support for STM32F411 Nucleo-64 boards

---
+++ src/stm32f411/forth/setup_mini_nucleo64.fs
@@ -0,0 +1,35 @@
+\ Copyright (c) 2020-2022 Travis Bemann
+\
+\ Permission is hereby granted, free of charge, to any person obtaining a copy
+\ of this software and associated documentation files (the "Software"), to deal
+\ in the Software without restriction, including without limitation the rights
+\ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+\ copies of the Software, and to permit persons to whom the Software is
+\ furnished to do so, subject to the following conditions:
+\ 
+\ The above copyright notice and this permission notice shall be included in
+\ all copies or substantial portions of the Software.
+\ 
+\ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+\ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+\ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+\ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+\ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+\ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+\ SOFTWARE.
+
+\ This is not actual Forth code, but rather setup directives for e4thcom to be
+\ executed from the root of the zeptoforth directory to initialize zeptoforth
+\ on an STM32F411 device.
+
+#include src/stm32f411/forth/clock_nucleo64.fs
+#include src/common/forth/basic.fs
+#include src/common/forth/module.fs
+#include src/common/forth/interrupt.fs
+#include src/common/forth/multicore.fs
+#include src/stm32f411/forth/erase.fs
+#include src/common/forth/systick.fs
+#include src/stm32f411/forth/int_io.fs
+#include src/stm32f411/forth/gpio.fs
+#include src/common/forth/task.fs
+
