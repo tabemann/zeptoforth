b12932b5f7a426330554ab9db48d08d321b5ffdd
Travis Bemann <tabemann@gmail.com>
2023-01-01 11:12:40 -0600

    Updated the copyright date to include 2023

--- src/common/legal.s
+++ src/common/legal.s
@@ -1,4 +1,4 @@
-@ Copyright (c) 2019-2022 Travis Bemann
+@ Copyright (c) 2019-2023 Travis Bemann
 @
 @ Permission is hereby granted, free of charge, to any person obtaining a copy
 @ of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,7 @@
 _license:
 	push {lr}
 	bl _cr
-	string_ln "Copyright (c) 2019-2022 Travis Bemann"
+	string_ln "Copyright (c) 2019-2023 Travis Bemann"
 	bl _type
 	string_ln "Copyright (c) 2013, 2021 Matthias Koch"
 	bl _type
--- src/m0/divide.s
+++ src/m0/divide.s
@@ -1,4 +1,4 @@
-@ Copyright (c) 2021-2022 Travis Bemann
+@ Copyright (c) 2021-2023 Travis Bemann
 @ Copyright (c) 2013 Matthias Koch
 @
 @ Permission is hereby granted, free of charge, to any person obtaining a copy
