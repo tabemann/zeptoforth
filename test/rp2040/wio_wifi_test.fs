8b2a6433571d939443a54c343c54135d1c95e239
Travis Bemann <tabemann@gmail.com>
2023-02-15 23:20:38 -0600

    Made some improvements

--- test/rp2040/wio_wifi_test.fs
+++ test/rp2040/wio_wifi_test.fs
@@ -26,28 +26,75 @@ begin-module wifi-test
   
   <esp-at> class-size buffer: device
   <wio-esp-at-spi> class-size buffer: intf
+  <esp-at-status> class-size buffer: status
   
   : reset-wifi ( -- ) [: reset-esp-at ;] device with-esp-at ;
   
   : run-test ( D: password D: ssid -- )
     <wio-esp-at-spi> intf init-object
     intf <esp-at> device init-object
+    <esp-at-status> status init-object
     200000 device esp-at-timeout!
     [: { D: password D: ssid device }
+      device esp-at-wifi-power@ cr ." Old WiFi power: " .
+      82 device esp-at-wifi-power!
+      device esp-at-wifi-power@ cr ." New WiFi power: " .
+      
       device disconnect-esp-at-wifi
       not-auto-connect station-mode device esp-at-wifi-mode!
-      password ssid device connect-esp-at-wifi
+      
+      begin
+        password ssid device [:
+          4 pick 4 pick 4 pick 4 pick 4 pick connect-esp-at-wifi
+        ;] try 0= nip nip nip nip nip
+      until
+      
       0 tcp 80 s\" google.com" device start-esp-at-single
-      [: cr ." >>>>>" cr drop type ;] device esp-at-recv-xt!
+      [: cr ." >>>>> " . ." >>>>>" cr type ;] device esp-at-recv-xt!
       s\" GET / HTTP/1.1\r\n" device single>esp-at
       s\" Host: google.com\r\n" device single>esp-at
-      s\" Accept: */*\r\n\r\n" device single>esp-at
+      s\" Accept: */*\r\n" device single>esp-at
+      s\" Connection: close\r\n\r\n" device single>esp-at
+      
       systick::systick-counter { start-time }
       begin
         device poll-esp-at
         systick::systick-counter start-time - 100000 >=
       until
+      
+      status device esp-at-status@
+      status esp-at-status.
+      
+\      600000 device esp-at-timeout!
+\      device close-esp-at-single
+\      200000 device esp-at-timeout!
+    
+      true device esp-at-multi!
+      
+      0 tcp 80 s\" google.com" 0 device start-esp-at-multi
+      s\" GET / HTTP/1.1\r\n" 0 device multi>esp-at
+      s\" Host: google.com\r\n" 0 device multi>esp-at
+      s\" Accept: */*\r\n\r\n" 0 device multi>esp-at
+      
+      systick::systick-counter { start-time }
+      begin
+        device poll-esp-at
+        systick::systick-counter start-time - 100000 >=
+      until
+
+      status device esp-at-status@
+      status esp-at-status.
+
+      0 device close-esp-at-multi
+    ;] device with-esp-at
+  ;
+  
+  : get-status ( -- )
+    [: { device }
+      status device esp-at-status@
+      status esp-at-status.
     ;] device with-esp-at
   ;
   
 end-module
+
