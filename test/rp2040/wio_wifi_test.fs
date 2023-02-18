2ec1846def690f744c42809c6d11cc8240f34a98
Travis Bemann <tabemann@gmail.com>
2023-02-17 21:12:56 -0600

    Improved the Wio RP2040 test by breaking it up into more words

--- test/rp2040/wio_wifi_test.fs
+++ test/rp2040/wio_wifi_test.fs
@@ -30,93 +30,112 @@ begin-module wifi-test
   
   : reset-wifi ( -- ) [: reset-esp-at ;] device with-esp-at ;
   
+  : configure-power { device -- }
+    disable-sleep-mode device esp-at-sleep!
+    device esp-at-wifi-power@ cr ." Old WiFi power: " dup .
+    
+    80 < if
+      10 0 ?do
+        82 device esp-at-wifi-power!
+        device esp-at-wifi-power@ cr ." New WiFi power: " dup .
+        80 >= if leave then
+      loop
+    then
+  ;
+  
+  : connect-to-ap { password ssid device -- }
+    device disconnect-esp-at-wifi
+    not-auto-connect station-mode device esp-at-wifi-mode!
+      
+    begin
+      password ssid device [:
+        4 pick 4 pick 4 pick 4 pick 4 pick connect-esp-at-wifi
+      ;] try nip nip nip nip nip
+      dup 0= if
+        drop true
+      else
+        dup ['] x-esp-at-error = if
+          drop false
+        else
+          ?raise
+        then
+      then
+    until
+  ;
+  
+  : station-info { device -- }
+    device esp-at-station-ipv4-addr@ if
+      cr ." Station IPv4 address: " type
+    else
+      2drop cr ." No station IPv4 address"
+    then
+    device esp-at-station-ipv6-gl-addr@ if
+      cr ." Station IPv6 global address: " type
+    else
+      2drop cr ." No station IPv6 global address"
+    then
+  ;
+  
+  : resolve-google { device -- }
+    resolve-ipv4-only s" google.com" device resolve-esp-at-ip if
+      cr ." google.com IPv4 address: " type
+    else
+      2drop cr ." No google.com IPv4 address"
+    then
+  ;
+  
+  : single-google-request { device -- }
+    [: cr ." >>>>> " . ." >>>>>" cr type ;] device esp-at-recv-xt!
+    0 tcp 80 s\" google.com" device start-esp-at-single
+    s\" GET / HTTP/1.1\r\n" device single>esp-at
+    s\" Host: google.com\r\n" device single>esp-at
+    s\" Accept: */*\r\n" device single>esp-at
+    s\" Connection: close\r\n\r\n" device single>esp-at
+    
+    systick::systick-counter { start-time }
+    begin
+      device poll-esp-at
+      systick::systick-counter start-time - 100000 >=
+    until
+    
+    status device esp-at-status@
+    status esp-at-status.
+   
+    device close-esp-at-single
+  ;
+  
+  : multi-google-request { device -- }
+    true device esp-at-multi!
+    
+    0 tcp 80 s\" google.com" 0 device start-esp-at-multi
+    s\" GET / HTTP/1.1\r\n" 0 device multi>esp-at
+    s\" Host: google.com\r\n" 0 device multi>esp-at
+    s\" Accept: */*\r\n\r\n" 0 device multi>esp-at
+    
+    systick::systick-counter { start-time }
+    begin
+      device poll-esp-at
+      systick::systick-counter start-time - 100000 >=
+    until
+
+    status device esp-at-status@
+    status esp-at-status.
+
+    0 device close-esp-at-multi
+  ;
+  
   : run-test ( D: password D: ssid -- )
     <wio-esp-at-spi> intf init-object
     intf <esp-at> device init-object
     <esp-at-status> status init-object
     200000 device esp-at-timeout!
     [: { D: password D: ssid device }
-      
-      disable-sleep-mode device esp-at-sleep!
-      
-      device esp-at-wifi-power@ cr ." Old WiFi power: " dup .
-      
-      80 < if
-        10 0 ?do
-          82 device esp-at-wifi-power!
-          device esp-at-wifi-power@ cr ." New WiFi power: " dup .
-          80 >= if leave then
-        loop
-      then
-      
-      device disconnect-esp-at-wifi
-      not-auto-connect station-mode device esp-at-wifi-mode!
-      
-      begin
-        password ssid device [:
-          4 pick 4 pick 4 pick 4 pick 4 pick connect-esp-at-wifi
-        ;] try nip nip nip nip nip
-        dup 0= if
-          drop true
-        else
-          dup ['] x-esp-at-error = if
-            drop false
-          else
-            ?raise
-          then
-        then
-      until
-      
-      device esp-at-station-ipv4-addr@ if
-        cr ." Station IPv4 address: " type
-      else
-        2drop cr ." No station IPv4 address"
-      then
-
-      device esp-at-station-ipv6-gl-addr@ if
-        cr ." Station IPv6 global address: " type
-      else
-        2drop cr ." No station IPv6 global address"
-      then
-      
-      [: cr ." >>>>> " . ." >>>>>" cr type ;] device esp-at-recv-xt!
-      
-\      0 tcp 80 s\" google.com" device start-esp-at-single
-\      s\" GET / HTTP/1.1\r\n" device single>esp-at
-\      s\" Host: google.com\r\n" device single>esp-at
-\      s\" Accept: */*\r\n" device single>esp-at
-\      s\" Connection: close\r\n\r\n" device single>esp-at
-\      
-\      systick::systick-counter { start-time }
-\      begin
-\        device poll-esp-at
-\        systick::systick-counter start-time - 100000 >=
-\      until
-\      
-\      status device esp-at-status@
-\      status esp-at-status.
-      
-\      600000 device esp-at-timeout!
-\      device close-esp-at-single
-\      200000 device esp-at-timeout!
-    
-      true device esp-at-multi!
-      
-      0 tcp 80 s\" google.com" 0 device start-esp-at-multi
-      s\" GET / HTTP/1.1\r\n" 0 device multi>esp-at
-      s\" Host: google.com\r\n" 0 device multi>esp-at
-      s\" Accept: */*\r\n\r\n" 0 device multi>esp-at
-      
-      systick::systick-counter { start-time }
-      begin
-        device poll-esp-at
-        systick::systick-counter start-time - 100000 >=
-      until
-
-      status device esp-at-status@
-      status esp-at-status.
-
-      0 device close-esp-at-multi
+      device configure-power  
+      password ssid device connect-to-ap
+      device station-info
+      device resolve-google
+      device single-google-request
+      device multi-google-request
     ;] device with-esp-at
   ;
   
