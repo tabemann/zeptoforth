e3078f71424c8d0c57f625d8bdb4edc1692d9f09
Travis Bemann <tabemann@gmail.com>
2023-04-11 23:16:32 -0500

    Reworked the lambda words to make them more efficient

--- src/common/forth/full_default.fs
+++ src/common/forth/full_default.fs
@@ -21,7 +21,6 @@
 \ This is not actual Forth code, but rather setup directives for e4thcom to be
 \ executed to load a "big" configuration
 
-#include src/common/forth/value.fs
 #include src/common/forth/closure.fs
 #include src/common/forth/temp.fs
 #include src/common/forth/temp_str.fs
