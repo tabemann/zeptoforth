d66f22d8b79656096b799b619afe3bb3ce2829dd
Travis Bemann <tabemann@gmail.com>
2023-05-06 23:51:15 -0500

    Added jump tables and fixed a bug which left garbage on the stack at bootup and resulted in ERROR-EMIT?-HOOK not being properly initialized

--- src/stm32l476/forth/int_io.fs
+++ src/stm32l476/forth/int_io.fs
@@ -247,7 +247,7 @@ begin-module int-io
     ['] do-emit error-emit-hook !
     ['] do-key? key?-hook !
     ['] do-emit? emit?-hook !
-    ['] do-emit? error-emit?-hook
+    ['] do-emit? error-emit?-hook !
     ['] do-flush-console flush-console-hook !
   ;
   
