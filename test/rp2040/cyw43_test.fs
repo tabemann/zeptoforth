4510d5c10293cb5338e16090a84f52b14b183e4a
Travis Bemann <tabemann@gmail.com>
2023-06-10 17:06:34 -0500

    Added some CYW43 test code

---
+++ test/rp2040/cyw43_test.fs
@@ -0,0 +1,45 @@
+\ Copyright (c) 2023 Travis Bemann
+\
+\ Permission is hereby granted, free of charge, to any person obtaining a copy
+\ of this software and associated documentation files (the "Software"), to deal
+\ in the Software without restriction, including without limitation the rights
+\ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+\ copies of the Software, and to permit persons to whom the Software is
+\ furnished to do so, subject to the following conditions:
+\ 
+\ The above copyright notice and this permission notice shall be included in
+\ all copies or substantial portions of the Software.
+\ 
+\ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+\ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+\ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+\ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+\ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+\ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+\ SOFTWARE.
+
+begin-module cyw43-test
+  
+  oo import
+  cyw43 import
+  
+  23 constant pwr-pin
+  24 constant dio-pin
+  25 constant cs-pin
+  29 constant clk-pin
+  
+  0 constant pio-addr
+  0 constant sm-index
+  pio::PIO0 constant pio-instance
+  
+  <cyw43> class-size buffer: my-cyw43
+  
+  : init-test ( -- )
+    dma-pool::init-dma-pool
+    cyw43-fw::data cyw43-fw::size pwr-pin clk-pin dio-pin cs-pin pio-addr sm-index pio-instance
+    <cyw43> my-cyw43 init-object
+    my-cyw43 init-cyw43
+    my-cyw43 run-cyw43
+  ;
+
+end-module
