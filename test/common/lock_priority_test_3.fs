848651e07052916f885467b5d46b924f83c228c1
Travis Bemann <tabemann@gmail.com>
2023-08-27 15:47:47 -0500

    Did some bugfixing for locks

--- test/common/lock_priority_test_3.fs
+++ test/common/lock_priority_test_3.fs
@@ -26,54 +26,85 @@ begin-module lock-test
   lock-size aligned-buffer: my-lock0
   lock-size aligned-buffer: my-lock1
   
+  : display-priority { index -- }
+    ." [priority " index (.) ." : " current-task task-priority@ .
+    ." saved: " current-task task-saved-priority@ (.)
+    ." ] "
+  ;
+  
   : test ( -- )
     my-lock0 init-lock
     my-lock1 init-lock
     0 [:
+      101 display-priority
       ." *A* "
       my-lock0 claim-lock
       my-lock1 claim-lock
+      201 display-priority
       ." *B* "
       5000 ms
+      1 display-priority
       my-lock0 release-lock
       my-lock1 release-lock
+      pause
       ." *C* "
+      -1 display-priority
     ;] 320 128 512 spawn { task0 }
     0 [:
+      102 display-priority
       1000 ms
       ." *X* "
       my-lock0 claim-lock
+      202 display-priority
       ." *Y* "
       5000 ms
+      2 display-priority
       my-lock0 release-lock
+      pause
       ." *Z* "
+      -2 display-priority
     ;] 320 128 512 spawn { task1 }
     0 [:
+      103 display-priority
       2000 ms
       ." *0* "
       my-lock0 claim-lock
+      203 display-priority
       ." *1* "
       5000 ms
+      3 display-priority
       my-lock0 release-lock
+      pause
       ." *2* "
+      -3 display-priority
     ;] 320 128 512 spawn { task2 }
-        0 [:
+    0 [:
+      104 display-priority
       500 ms
       ." *a* "
       my-lock1 claim-lock
+      204 display-priority
       ." *b* "
       5000 ms
+      4 display-priority
       my-lock1 release-lock
+      pause
       ." *c* "
+      -4 display-priority
     ;] 320 128 512 spawn { task3 }
     0 [:
+      105 display-priority
       1500 ms
       ." *x* "
       my-lock1 claim-lock
+      205 display-priority
       ." *y* "
       5000 ms
+      5 display-priority
       my-lock1 release-lock
+      pause
       ." *z* "
+      -5 display-priority
     ;] 320 128 512 spawn { task4 }
     0 task0 task-priority!
     1 task1 task-priority!
