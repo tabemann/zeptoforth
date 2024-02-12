75ca17610fa3a6c0adb9a252d7bb3ef79931a3ae
Travis Bemann <tabemann@gmail.com>
2024-02-11 19:55:30 -0600

    Restored max-iteration to 16

--- test/rp2040/julia_st7735s_cycle.fs
+++ test/rp2040/julia_st7735s_cycle.fs
@@ -49,7 +49,7 @@ begin-module julia
   <st7735s> class-size buffer: my-display
   
   \ The maximum number of iterations
-  12 constant max-iteration
+  16 constant max-iteration
 
   \ Color table
   max-iteration 1+ cell align aligned-buffer: colors
