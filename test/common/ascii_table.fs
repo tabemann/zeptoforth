c24a0a68bcc23870c2950841e476faa512b26b8c
Travis Bemann <tabemann@gmail.com>
2024-02-24 09:32:48 -0600

    Added an implicitly-compiled ASCII table

---
+++ test/common/ascii_table.fs
@@ -0,0 +1,42 @@
+\ Copyright (c) 2024 Travis Bemann
+\
+\ Permission is hereby granted, free of charge, to any person obtaining a copy
+\ of this software and associated documentation files (the "Software"), to deal
+\ in the Software without restriction, including without limitation the rights
+\ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+\ copies of the Software, and to permit persons to whom the Software is
+\ furnished to do so, subject to the following conditions:
+\ 
+\ The above copyright notice and this permission notice shall be included in
+\ all copies or substantial portions of the Software.
+\ 
+\ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+\ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+\ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+\ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+\ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+\ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+\ SOFTWARE.
+
+16 0 ?do
+  cr
+  8 0 ?do
+    i 16 * j + { c }
+    c h.2 space
+    c $20 >= c $7F < and if c emit space space
+    else
+      c $10 < if
+        s" NULSOHSTXETXEOTENQACKBELBS HT LF VT FF CR SO SI "
+        drop c 3 * + 3 type
+      else
+        c $20 < if
+          S" DLEDC1DC2DC3DC4NAKSYNETBCANEM SUBESCFS GS RS US "
+          drop c $10 - 3 * + 3 type
+        else
+          ." DEL"
+        then
+      then
+    then
+    space
+  loop
+loop
