61c310d99ad49e67ecaa8d0ad4ee04649b7fdc59
Travis Bemann <tabemann@gmail.com>
2024-08-27 23:50:34 -0500

    Fixed dictionary issues and BOOTSEL

--- src/rp2350/common.s
+++ src/rp2350/common.s
@@ -148,18 +148,7 @@ _reboot:
 
 1:      b 2f
 2:      b 1b
-        
-@        movs r2, #0
-@	movs r1, #RT_FLAG_FUNC_ARM_SEC
-@	ldr r0, ='R | ('B << 8)
-@	ldrh r3, [r2, #0x16]
-@	blx r3
-@	movs r4, r0
-@	ldr r0, =0x0 @ | 0x0100
-@	movs r1, #0
-@        movs r2, #0
-@        movs r3, #0
-@	blx r4
+
 	pop {r4, pc}
 	end_inlined
 
@@ -178,7 +167,19 @@ _bootsel:
         movs r2, #0
         movs r3, #0
 	blx r4
-        pop {r4, lr}
+
+        ldr r0, =WATCHDOG_BASE
+        ldr r1, =ALIAS_SET
+        orrs r0, r1
+        ldr r1, =WATCHDOG_CTRL_TRIGGER
+        ldr r2, =0
+        orrs r1, r2
+        str r1, [r0, #WATCHDOG_CTRL]
+
+1:      b 2f
+2:      b 1b
+
+        pop {r4, pc}
 	end_inlined
 
 	@@ Execute a PAUSE word, if one is set
