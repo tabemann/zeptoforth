5a9a9e1a7d6bc9ba3764e212a572c02e3bbfaee8
serialcomms <serialcomms@outlook.com>
2024-11-26 17:49:31 +0000

    Add files via upload

--- usb_console.fs
+++ usb_console.fs
@@ -122,19 +122,19 @@ console import
       
         ep1-to-host busy? @ if
 
-            transmit-character write-tx             \ add byte to already-running queue
+            transmit-character write-tx             \ add byte to already-running queue ( not-full queue has at least 1 byte capacity )
 
             ep1-to-host busy? @ not if              \ EP may have finished by the (short) time it takes to get here
 
-            ep1-start-queue-runner-to-host
+            ep1-start-queue-runner-to-host          \ 
 
             then
         
         else
 
-            ep1-to-host 1 ' transmit-character usb-build-data-packet  \ skip queue as not already running
+            ep1-to-host 1 ' transmit-character usb-build-data-packet    \ skip queue as not already running
 
-            ep1-to-host 1 send-data-packet
+            ep1-to-host 1 send-data-packet                              \ send a 1-byte packet to host
       
         then
 
@@ -152,9 +152,9 @@ console import
 
         ep1-to-pico queue-long? @ if                \ is queue previously marked as long ( < 64 free bytes remaining ) ?
 
-        rx-count 63 > if                            \ did read-rx make 64 bytes or more free ?
+        rx-count 63 > if                            \ did read-rx make 64 bytes or more available to receive ?
 
-            false ep1-to-pico queue-long? !         \ cancel queue-long
+            false ep1-to-pico queue-long? !         \ cancel queue-long ( if previously set by ep1-handler-to-pico )
 
             ep1-to-pico 64 usb-receive-data-packet  \ start another receive packet which we now have rx capacity for
    
