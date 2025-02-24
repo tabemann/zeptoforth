e1f08b884584a971c9ef28b34da28bef39220759
Travis Bemann <tabemann@gmail.com>
2025-02-23 18:33:59 -0600

    Fixed copy-and-paste errors in dma.fs for the RP2040 and RP2350

--- src/rp2040/forth/dma.fs
+++ src/rp2040/forth/dma.fs
@@ -1,4 +1,4 @@
-\ Copyright (c) 2023 Travis Bemann
+\ Copyright (c) 2023-2025 Travis Bemann
 \
 \ Permission is hereby granted, free of charge, to any person obtaining a copy
 \ of this software and associated documentation files (the "Software"), to deal
@@ -203,7 +203,7 @@ begin-module dma
   ;
 
   \ Incrementing ring buffer write
-  : INCR_RING_BUFFER_READ ( low-ring-bits -- mode )
+  : INCR_RING_BUFFER_WRITE ( low-ring-bits -- mode )
     CH_CTRL_TRIG_RING_SIZE_LSB lshift CH_CTRL_TRIG_RING_SIZE_MASK and
     [ INCR_BUFFER_WRITE CH_CTRL_TRIG_RING_SEL or ] literal or
   ;
