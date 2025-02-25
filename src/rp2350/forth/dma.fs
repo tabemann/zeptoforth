07b56b4b0457a18ddb99e569b288d90dad2c21dd
Travis Bemann <tabemann@gmail.com>
2025-02-24 19:31:34 -0600

    Made a fix to CH_CTRL_TRIG_RING_SIZE_LSB typos

--- src/rp2350/forth/dma.fs
+++ src/rp2350/forth/dma.fs
@@ -84,7 +84,7 @@ begin-module dma
     12 bit constant CH_CTRL_TRIG_RING_SEL
 
     \ Ring lower bit count LSB
-    8 bit constant CH_CTRL_TRIG_RING_SIZE_LSB
+    8 constant CH_CTRL_TRIG_RING_SIZE_LSB
 
     \ Ring lower bit count mask
     $F CH_CTRL_TRIG_RING_SIZE_LSB lshift constant CH_CTRL_TRIG_RING_SIZE_MASK
