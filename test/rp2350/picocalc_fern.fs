39261e21ee183e0c348b75d16cec2da2618a6f6a
Travis Bemann <tabemann@gmail.com>
2025-06-29 00:17:00 -0500

    Fixed an omission

--- test/rp2350/picocalc_fern.fs
+++ test/rp2350/picocalc_fern.fs
@@ -1,5 +1,5 @@
 \ Copyright (c) 2025 Travis Bemann
-\
+\ 
 \ Permission is hereby granted, free of charge, to any person obtaining a copy
 \ of this software and associated documentation files (the "Software"), to deal
 \ in the Software without restriction, including without limitation the rights
@@ -26,7 +26,7 @@ begin-module fern
   tinymt32 import
   float32 import
   
-  : draw-fern ( -- )
+  : draw-fern ( seed -- )
     tinymt32-size [: { seed prng }
       seed prng tinymt32-init
       prng tinymt32-prepare-example
