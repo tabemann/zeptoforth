b1acd276c0d78f6330d0a186daa0fac1eaba8ba9
Travis Bemann <tabemann@gmail.com>
2026-07-11 21:47:54 -0500

    Updated docs

--- epub/_static/language_data.js
+++ epub/_static/language_data.js
@@ -1,7 +1,8 @@
 /*
  * This script contains the language-specific data used by searchtools.js,
- * namely the list of stopwords, stemmer, scorer and splitter.
+ * namely the set of stopwords, stemmer, scorer and splitter.
  */
 
-var stopwords = ;
+const stopwords = new Set();
+window.stopwords = stopwords;  // Export to global scope
 
