6bac0e1f427e2a8c246d375cad65e5e994930ab5
Travis Bemann <tabemann@gmail.com>
2026-07-18 12:35:17 -0500

    Added yet another L-system

---
+++ test/common/lindenmayer_quad_snowflake.fs
@@ -0,0 +1,34 @@
+\ Copyright (c) 2026 Travis Bemann
+\ 
+\ Permission is hereby granted, free of charge, to any person obtaining a copy
+\ of this software and associated documentation files (the "Software"), to deal
+\ in the Software without restriction, including without limitation the rights
+\ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+\ copies of the Software, and to permit persons to whom the Software is
+\ furnished to do so, subject to the following conditions:
+\ 
+\ The above copyright notice and this permission notice shall be included in
+\ all copies or substantial portions of the Software.
+\ 
+\ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+\ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+\ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+\ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+\ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+\ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+\ SOFTWARE.
+
+begin-module quad-snowflake
+  
+  lindenmayer import
+  
+  90 turn t+
+  -90 turn t-
+  step F
+  0,1 16,0 f/ 4 :rcolor-forward-step F F t+ F t- F t- F t+ F ;step
+  
+  3 0,0 255 255 255 120,0 -120,0 0 :axiom draw-quad-snowflake
+    F F t+ F F t+ F F t+ F F
+  ;axiom
+  
+end-module
