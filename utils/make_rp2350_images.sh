6e89db97ddabb994f89f920e5e234b0f8d0e0cca
Travis Bemann <tabemann@gmail.com>
2026-09-05 15:02:32 -0500

    Fixed a bug

--- utils/make_rp2350_images.sh
+++ utils/make_rp2350_images.sh
@@ -55,6 +55,7 @@ issue_bootsel ${PORT}
 
 flash_rp ${RP2350_KERNEL} ${BLOCK_DEVICE} ${FILESYSTEM}
 ${DIR}/make_uf2_image.sh ${VERSION} rp2350 ${PORT} full_usb
+issue_bootsel ${PORT}
 
 flash_rp ${RP2350_KERNEL} ${BLOCK_DEVICE} ${FILESYSTEM}
 ${DIR}/make_uf2_image.sh ${VERSION} rp2350 ${PORT} full_msc_only
